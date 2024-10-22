fce9785ec085bb97f1daf8a4e00e1b70220081fd
I528616 <shivani.pandey03@sap.com>
2024-10-22 17:38:22 +0530

    JwtAuthentication

---
+++ src/main/java/com/apiproject/ServiceBookingSystem/configs/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.apiproject.ServiceBookingSystem.configs;
+
+import com.apiproject.ServiceBookingSystem.services.jwt.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+    @Autowired
+    private JwtRequestFilter requestFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/authenticate","/company/sign-up","/client/sign-up","/ads","/search/{service}").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/api/**")
+                .authenticated().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/apiproject/ServiceBookingSystem/services/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.apiproject.ServiceBookingSystem.services.jwt;
+
+import com.apiproject.ServiceBookingSystem.entity.User;
+import com.apiproject.ServiceBookingSystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.Null;
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user= userRepository.findFirstByEmail(email);
+        if(user== null) throw new UsernameNotFoundException("Username not found",null);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),new ArrayList<>());
+    }
+}
