6d967ab69913577a22b9e47a5fb395ab51ebd73d
BarshaBits <161912217+BarshaBits@users.noreply.github.com>
2024-11-05 20:54:44 +0530

    list update ads

--- src/main/java/com/apiproject/ServiceBookingSystem/repository/AdRepository.java
+++ src/main/java/com/apiproject/ServiceBookingSystem/repository/AdRepository.java
@@ -3,8 +3,11 @@ package com.apiproject.ServiceBookingSystem.repository;
 import com.apiproject.ServiceBookingSystem.entity.Ad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.apiproject.ServiceBookingSystem.dto.AdDTO;
+
+import java.util.List;
 
 @Repository
-public interface AdRepository extends JpaRepository<Ad, Long> {
+public interface AdRepository extends JpaRepository<Ad, Long> { List<Ad> findAllByUserId(long userId);
 
 }
